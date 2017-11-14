6aaa85fc0112e6576d48609c73a6997264ee62ed
Idham Rahadian <Idham Rahadian@DESKTOP-B0S9MV9>
2017-11-14 21:02:48 +0700

    PBO11-10116476-LATIHANUTS1

--- src/com/latihan1/view/MainView.java
+++ src/com/latihan1/view/MainView.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.idhamrahadian.view;
+package com.latihan1.view;
 
-import com.idhamrahadian.model.ModelMahasiswa;
+import com.latihan1.model.ModelMahasiswa;
 import javax.swing.JOptionPane;
 
 /**
