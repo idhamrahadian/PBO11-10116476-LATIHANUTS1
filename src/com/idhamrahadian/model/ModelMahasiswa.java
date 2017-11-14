bdd1c8aaa594d8f3999469b339da13408fd3b941
Idham Rahadian <Idham Rahadian@DESKTOP-B0S9MV9>
2017-11-14 08:37:05 +0700

    PBO11-10116476-LATIHANUTS1

---
+++ src/com/idhamrahadian/model/ModelMahasiswa.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.idhamrahadian.model;
+
+/**
+ *
+ * @author Idham Rahadian
+ */
+public class ModelMahasiswa {
+    
+    private String  nim;
+    private String nama;
+    
+    public ModelMahasiswa(String nim, String nama){
+        
+        this.nim = nim;
+        this.nama = nama;
+        
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    
+}
